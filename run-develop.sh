3fb1e0f54ac8b37dba1c023e424a02a6b2589098
nhuthuynh96 <nhuthm080280@gmail.com>
2020-04-18 18:09:26 +0700

    add config file

--- run-develop.sh
+++ run-develop.sh
@@ -13,7 +13,7 @@ RETVAL=0
 moduleName="student-portal"
 JAVA_OPTS="-Xmx300m -Xms128m -Dfile.encoding=UTF8 -Dspring.config.location=config/application-develop.properties"
 MODULE_OPTS=""
-FOLDER="./target"
+FOLDER="/home/le_tran0590/studentportal"
 JAR_FILE=student-portal-1.0.jar
 
 start() {

8b2d5eebc25474ec6d3ceb8ae2404e66df5eef9b
nhuthuynh96 <nhuthm080280@gmail.com>
2020-04-18 18:26:44 +0700

    add nohup

--- build_run.sh
+++ build_run.sh
@@ -1,2 +1,2 @@
 mvn clean install
-java -jar target/student-portal-1.0.jar --spring.config.location=file:////home/le_tran0590/studentportal/config/application-develop.properties >> studentportal.log
+nohup java -jar target/student-portal-1.0.jar --spring.config.location=file:////home/le_tran0590/studentportal/config/application-develop.properties >> studentportal.log
